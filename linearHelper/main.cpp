4db11b9820e56cea260c94a9fa407b2137dda2db
Damirov Eldar <eldardamirov75@mail.ru>
2020-02-07 16:47:38 +0300

    Initial Commit

---
+++ linearHelper/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  linearHelper
+//
+//  Created by Эльдар Дамиров on 07.02.2020.
+//  Copyright © 2020 Эльдар Дамиров. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
